e06b04a8053c0a206546f6ccc3bb1c6d417dd892
Teodor57 <83972168+Teodor57@users.noreply.github.com>
2022-11-23 13:14:49 +0200

    Function DB

---
+++ function.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PharmacyStore
+{
+    class function
+    {
+        protected SqlConnection getConnection()
+        {
+           SqlConnection con =new SqlConnection();
+          
+            con.ConnectionString = "Data Source = DESKTOP-QJEI5AP; Initial Catalog = Pharmacy_DataBase; Integrated Security = True; Connect Timeout = 60; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            return con;
+        }
+
+        public DataSet getData(String query)
+        {
+            SqlConnection con = getConnection();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = query;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+
+        public void setData(String query, String msg)
+        {
+          SqlConnection con =getConnection();
+           
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = con;
+            con.Open();
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show(msg,"Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+    }
+}
