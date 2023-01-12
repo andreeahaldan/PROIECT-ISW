368e640c5a5cb24f8c55a71997a3745b48e06afb
Victor Aldica <58776972+SirVicCreamy@users.noreply.github.com>
2023-01-12 21:35:19 +0200

    Added injection checker

---
+++ InjectionChecker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyStore
+{
+    public class InjectionChecker
+    {
+
+
+        public InjectionChecker()
+        {
+
+        }
+        public bool WordCheck(String text)
+        {
+
+            String[] SQLCommands = { "DROP", "INSERT", "DELETE", "ALTER", "TABLE" };
+            text = text.ToUpper();
+            bool risk = false;
+            foreach (String SQLCommand in SQLCommands)
+            {
+                if (text.Contains(SQLCommand))
+                {
+                    risk = true;
+                }
+
+
+            }
+            return risk;
+        }
+    }
+}
